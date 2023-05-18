98e61ac73bb8ded12c24604cfa91d1f2e0a8d8f2
Menta <simcictilen.gmail.com>
2023-05-18 16:16:36 +0200

    prove inserimento immagine

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return{
+            message: 'Hello Vue!',
+            img: 'img/logo.svg'
+        }
+    }
+
+}).mount('#app')
+
+let elSelector = document.getElementById('app');
+
+console.log(elSelector);
+
+elSelector.innerHTML += `<img src="{{ img }}" alt="ciao"> `;
